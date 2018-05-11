9ed8f815c9c420776bd67f88a19cd96ed054ab3c
lyth031 <lyth031@outlook.com>
2018-05-11 16:53:59 +0800

    debug

--- model.py
+++ model.py
@@ -47,9 +47,8 @@ def cnn_model(input_images,train_logical=True):
     pool3 = tf.nn.max_pool(relu_conv3, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1],padding='SAME', name='pool_layer3')
 
     pool_reshape = pool3.get_shape().as_list()
-    print(pool_reshape)
     nodes = pool_reshape[1]*pool_reshape[2]*pool_reshape[3]
-    reshaped_output = tf.reshape(pool3, [pool_reshape[0], nodes])
+    reshaped_output = tf.reshape(pool3, [-1, nodes])
 
     with tf.variable_scope('full1'):
         full_weight1 = tf.get_variable(name='weights', 
